11b8834d4671f1f3b9339adafabc493124c3f48c
KITT-KAT13 <Collinsnyade@gmaail.com>
2025-10-21 17:52:31 +0000

    first commit

---
+++ Index.py
@@ -0,0 +1,5 @@
+# Printing in python
+print("My name is Collins Nyade")
+print("I am twelve years old")
+print("I atend school at Crystal Heights Int'l School")
+print(5*5 + 6)
---
+++ Print.py
@@ -0,0 +1,11 @@
+#Creating different variables
+f = 5
+j = "Luke"
+print(f)
+print(j)
+
+#Taking user input
+
+name = input("Enter your name: ")
+
+print("\nHello", name, "\nWelcome to this website")
