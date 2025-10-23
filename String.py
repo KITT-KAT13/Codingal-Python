7fc72db007b6626504f6775b7210dc08aaf85418
KITT-KAT13 <Collinsnyade@gmaail.com>
2025-10-23 17:55:18 +0000

    r

---
+++ String.py
@@ -0,0 +1,18 @@
+# checking the data types of different variables
+a = 56
+b = "codingal"
+c = 32.5
+d = True
+x = ["coding", "programing", "python," ,"HTML"]
+ 
+y = {"coding", "programing", "python," ,"HTML"}
+
+z = ("coding", "programing", "python," ,"HTML")
+
+print("type a :",type(a))
+print("type b :",type(b))
+print("type c :",type(c))
+print("type d :",type(d))
+print("type x :",type(x))
+print("type y :",type(y))
+print("type z :",type(z))
---
+++ Tuple.py
@@ -0,0 +1,28 @@
+name = "Collins"
+age = 312
+is_student = True
+weight = 34.4
+print("Name :", name)
+print("Data type of name is:", type(name))
+
+print("Age :", age)
+print("Data type of age is:", type(age))
+
+print("is_student :", is_student)
+print("Data type of is_student is:", type(is_student))
+
+print("weight :", weight)
+print("Data type of weight is:", type(weight))
+
+
+
+#Type Casting to convert the Datatype of variables
+
+print("\nAfter Typecasting...")
+age = str(age)
+print(age)
+print("Data type of age is:", type(age))
+
+weight = float(weight)
+print(weight)
+print("Data type of weight is:", type(weight))
