9cd2c19865190fed9a6658bb06e201d3d8209e6d
KITT-KAT13 <Collinsnyade@gmaail.com>
2025-11-06 17:55:02 +0000

    the

---
+++ AND,OR operators/AND.py
@@ -0,0 +1,23 @@
+x = 34
+y = 56
+z = 0
+
+if x and y and z:
+    print("all the numbers have boolean values as True")
+else:
+     print("At least one number has boolean values as False")
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+        print("Either of the numbers is greater than zero")
+else:
+      print("No number is greater than zero")
+
+if b > 0 or c > 0:
+       print("Either of the numbers is greater than zero")
+else:
+      print("No number is greater than zero")
+
---
+++ AND,OR operators/BMI.py
@@ -0,0 +1,16 @@
+height = float(input("Enter your height in cm: "))
+weight = float(input("Enter your weight in kgs: "))
+
+BMI = weight / (height/100)**2
+
+print("your BMI is: ",BMI)
+
+if BMI <= 18.4:
+    print("You are underweight. ")
+elif BMI <= 24.4:
+     print("You are health") 
+elif BMI <= 29.9:
+     print("You are overweight")      
+else:
+     print("you are serverely overweight")
+
