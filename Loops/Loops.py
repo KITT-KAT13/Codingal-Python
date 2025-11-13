7ed8dcbfa14c10f469fcb09e876013685f9b9fb0
KITT-KAT13 <Collinsnyade@gmaail.com>
2025-11-13 17:54:19 +0000

    t

---
+++ Loops/Loops.py
@@ -0,0 +1,9 @@
+num = int(input("Please enter the number whose sum you want to find:"))
+sum = 0
+
+#Iterate for n+1 times: i = 1 to n+ 1
+
+for i in range(1, num+1): 
+      sum = sum + i
+      print("\nSum =", sum)
+
---
+++ Loops/Reverse.py
@@ -0,0 +1,8 @@
+string = input("Please enter your own string:")
+
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("\n The orginal string :", string) 
+print("The reversed string: ",string2)   
