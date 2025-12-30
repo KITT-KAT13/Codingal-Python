e21cc0081e778251bb63de072345ea8265720a44
KITT-KAT13 <Collinsnyade@gmaail.com>
2025-12-30 17:45:09 +0000

    hello

---
+++ List/Index.py
@@ -0,0 +1,13 @@
+empty_list =[] 
+
+print()
+
+numbers =[1,2,3,4,5,6,7,8]
+print(numbers)
+
+triple = [1,6,7,1] * 6
+print(triple)
+ 
+green = [1,2,3,4,5,6,7,8,9,0]
+green = green[:: -1] 
+print(green, "/n")
