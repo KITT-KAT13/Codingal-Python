ed65feff33f2b3f246422f1af9f522e9a8e3ff82
KITT-KAT13 <Collinsnyade@gmaail.com>
2026-01-20 17:58:21 +0000

    r

---
+++ Object Oriented Programming/Index.py
@@ -0,0 +1,4 @@
+class student:
+    grade = 7
+    print("My name is Collins and i am in", grade)
+ob =student()
---
+++ Object Oriented Programming/Index2.py
@@ -0,0 +1,26 @@
+class vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+Audi = vehicle(540, 100)
+
+#accessing variables
+
+print("Audi speed is:", Audi.max_speed)
+print("Audi mileage is:", Audi.mileage)
+
+Toyota = vehicle(500, 100)
+
+#accessing variables
+
+print("Toyota speed is:", Toyota.max_speed)
+print("Toyota mileage is:", Toyota.mileage)
+
+
+
+
+
+
+
+                                                                           
